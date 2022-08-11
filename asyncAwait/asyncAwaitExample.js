832699b790d88047fe9fc7196dc3f39558a2e459
Ningzhi <ningzhi04@hotmail.com>
2022-08-11 14:18:22 +0800

    finished asynch await tutorial

--- asyncAwait/asyncAwaitExample.js
+++ asyncAwait/asyncAwaitExample.js
@@ -17,16 +17,22 @@ function processRequest(response){
 }
 
 // you need to have some kind of function that you are awaiting code is inside 
-
 async function doWork(){
-    const response = await makeRequest('Google')
-    /*
-    await: the code should wait until this makeRequest is finished, then it will execute the next thing 
-    once the interpretor hits this await statement, it will just leave this function, do other work inside the programe,
-    as soon as the makeRequest finishes, executing, it will comeback into here and return the result into the response 
-    variable. 
-    */
-   console.log('Response Recieved')
-   const processedResponse = await processedResponse(response)
-   console.log(processedResponse)
+    try{
+        const response = await makeRequest('Google')
+        /*
+        await: the code should wait until this makeRequest is finished, then it will execute the next thing 
+        once the interpretor hits this await statement, it will just leave this function, do other work inside the programe,
+        as soon as the makeRequest finishes, executing, it will comeback into here and return the result into the response 
+        variable. 
+        */
+       console.log('Response Recieved')
+       const processedResponse = await processedResponse(response)
+       console.log(processedResponse)
+    } catch (error){
+        console.log(error)
+    }
+
 }
+
+doWork()
